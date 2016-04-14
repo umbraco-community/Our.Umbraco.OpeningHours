03ff0f4158f0631ef86f9eb1f0e0ba8d9f57a039
Anders Bjerner <abjerner@skybrud.dk>
2016-04-14 19:21:24 +0200

    Updated the assembly name to match the solution/project name

--- src/Our.Umbraco.OpeningHours/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.OpeningHours/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpeningHours.Umbraco.Source")]
+[assembly: AssemblyTitle("Our.Umbraco.OpeningHours")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpeningHours.Umbraco.Source")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Our.Umbraco.OpeningHours")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
