d2f5e518a287c41f4cffa08ff6556ebadbb89d9a
Anders Bjerner <abjerner@skybrud.dk>
2016-06-13 23:12:09 +0200

    A lot of rewrite

---
+++ src/Our.Umbraco.OpeningHours/App_Plugins/OpeningHours/Directives/DatePicker.js
@@ -0,0 +1,27 @@
+﻿angular.module('umbraco.directives').directive('ohDatePicker', function () {
+    return {
+        scope: {
+            model: '='
+        },
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/App_Plugins/OpeningHours/Views/openingHours.datePicker.html',
+        link: function (scope) {
+
+            // Initialize the configuration of Umbraco's date picker
+            scope.datePicker = {
+                view: 'datepicker',
+                config: { pickDate: true, pickTime: false, pick12HourFormat: false, format: 'YYYY-MM-DD' },
+                value: scope.model
+            };
+
+            // Watch the value of the date picker so we can put it back into "model"
+            scope.$watch(function () {
+                return scope.datePicker.value;
+            }, function (value) {
+                scope.model = value;
+            });
+
+        }
+    };
+});
